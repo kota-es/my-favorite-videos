bfcb32c0963683bff006ac5d3a270890aa89a0f5
kota-es <info@k-3110.com>
2019-10-14 15:14:24 +0900

    ビデオ登録機能実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,21 @@
 class VideosController < ApplicationController
   def index
+    @videos = current_user.videos.all.order("created_at DESC")
   end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    current_user.videos.create(video_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:word, :title, :url, :note)
+  end
+
 end

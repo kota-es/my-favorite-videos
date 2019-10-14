fd936fd59c9830017c096a572c635a1c81b22828
kota-es <info@k-3110.com>
2019-10-14 14:36:59 +0900

    Iinitail Commit

---
+++ app/assets/javascripts/keyword.js
@@ -0,0 +1,25 @@
+$(document).on('turbolinks:load', function(){
+  var btn = $("#r-btn") 
+  var input = $("#video_word")
+  var speech = new webkitSpeechRecognition();
+  speech.lang = "ja";
+
+  $(btn).on("click", function(){
+    $(input).val(""); 
+    $(btn).text("キーワードを話してください")
+    speech.start();
+  });
+
+  speech.addEventListener('result', function(e){
+    var script = e.results[0][0].transcript;
+    console.log(script);
+    $(input).val(script);
+    $(btn).text('やり直す場合は再度クリック');
+  });
+  
+  speech.onsoundstart = () => {
+    $(btn).text('聞き取り中...');
+  };
+  
+
+});

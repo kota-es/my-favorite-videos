1a7431752929408f58b2182f8f57c5e5923bcd19
kota-es <info@k-3110.com>
2019-10-14 15:05:49 +0900

    再生ページの実装

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,11 @@
 class Video < ApplicationRecord
   belongs_to :user
+
+  def video_count
+    if current_user.videos.length > 0
+      @count = current_user.videos.length
+    else
+      @count = "0"
+    end 
+  end
 end

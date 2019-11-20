577e6f71487d81bd5824e0cf288b8cc583918ad9
kota-es <info@k-3110.com>
2019-11-20 12:21:06 +0900

    不要な記述の削除

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -15,10 +15,7 @@ $(document).on('turbolinks:load', function(){
   });
   
   speech.addEventListener('result', function(e){
-    var script = e.results[0][0].transcript;
-    console.log(e)
-    console.log(script);
-    
+    var script = e.results[0][0].transcript;  
     if(script == 'ストップ'){
       VideoStop();
     }else{
